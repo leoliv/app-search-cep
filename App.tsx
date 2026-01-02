d5f2fe85b0c3a133306a7393a9af419af22e2729
Leo Oliveira <leoliv95@gmail.com>
2026-01-02 15:43:10 -0300

    feature: create area interface

--- App.tsx
+++ App.tsx
@@ -1,13 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { InputBox } from "./src/components/InputBox";
+import { Button } from "./src/components/Button";
 
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
         <StatusBar style="auto" />
+        <InputBox />
+        <View style={styles.areaButton}>
+          <Button
+            name="Buscar"
+            color="#0046afff"
+            onPress={() => console.log("Buscou!")}
+          />
+          <Button
+            name="Limpar"
+            color="#bb2803ff"
+            onPress={() => console.log("Limpou!")}
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -18,6 +32,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+  },
+  areaButton: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
   },
 });
